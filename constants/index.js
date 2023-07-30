be9296422271708c6c92107979f76db14cc9a7ce
Hamzeh <106542619+h4mz3h@users.noreply.github.com>
2023-07-30 16:02:19 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x49e4f0f6edb13b33c077fcc1c954aaa5fd58354b";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
